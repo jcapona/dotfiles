7746251e97142927b902941b6136329167197297
Jorge Capona <jcapona@gmail.com>
2021-03-06 14:37:24 -0300

    Create nvim config folder if it doesn't exist

--- install.sh
+++ install.sh
@@ -70,6 +70,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir -p ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 echo "Install the plugins by opening vim and running :PlugInstall"
 
