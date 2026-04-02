832970cd29512b3bf530a689cb548475b9090bff
Jorge Capona <jcapona@gmail.com>
2026-04-01 21:16:44 -0300

    Add todo-comments plugin for TODO/FIXME highlighting

---
+++ nvim/lua/config/plugins/todo-comments.lua
@@ -0,0 +1,7 @@
+-- https://github.com/folke/todo-comments.nvim
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {},
+}
